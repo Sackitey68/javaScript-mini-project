9891ebff3d52b41128aa3540a5ac110748e91ba8
Sackitey68 <sackitey68@gmail.com>
2024-05-02 04:15:30 +0000

    Implement the logic of the game

--- Guess-My-Number-Game/script.js
+++ Guess-My-Number-Game/script.js
@@ -1,3 +1,29 @@
 'use strict';
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when number is correct
+  if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🏆 Correct Number';
+    document.querySelector('body').style.backgroundColor = '#253314';
+  } else if (score > 1) {
+    if (guess > secretNumber) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = '⛔ You lost';
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('body').style.backgroundColor = '#FF0000';
+  }
+});
