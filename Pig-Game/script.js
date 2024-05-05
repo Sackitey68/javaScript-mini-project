85b9505cdb1d6a41a8c7aba2f2e0e2e48eb32046
Sackitey68 <sackitey68@gmail.com>
2024-05-05 15:41:13 +0000

    implement dry principle

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -5,21 +5,36 @@ const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-const btnNewGame = document.querySelector('.btn--new');
+
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
 
-let currentScore = 0;
-let activePlayer = 0;
-let scores = [0, 0];
-let playing = true;
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNewGame = document.querySelector('.btn--new');
+const diceEl = document.querySelector('.dice');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
+let scores, currentScore, activePlayer, playing;
+// starting condition
+const gameStart = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const start = function () {};
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+gameStart();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -28,10 +43,6 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
-
-//Hide the dice when the game loads
-const diceEl = document.querySelector('.dice');
-diceEl.classList.add('hidden');
 
 //Rolling the Dice
 btnRoll.addEventListener('click', function () {
@@ -62,7 +73,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // check if active player score greater or equal to 100 and declare that player a winner
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the Game
       playing = false;
       diceEl.classList.add('hidden');
@@ -78,18 +89,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNewGame.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-
-  currentScore = 0;
-  activePlayer = 0;
-  scores = [0, 0];
-  playing = true;
-});
+btnNewGame.addEventListener('click', gameStart);
