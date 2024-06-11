e33de5e39eab39b5c0b2c57c44dfa52179e1da4f
Sackitey68 <sackitey68@gmail.com>
2024-06-11 08:55:32 +0000

    Implement adding of expenses

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -14,17 +14,40 @@ addBtn.addEventListener("click", function () {
   const amount = Number(amountInput.value.trim());
   const date = dateInput.value.trim();
 
- 
   // input validation
-  if (nameInput === "" || amountInput === "" || dateInput === "") {
+  if (expenseName === "" || amount === "" || date === "") {
     alert("Please fill all the fields");
     return;
   }
 
-  if (isNaN(amountInput) || amountInput <= 0) {
+  if (isNaN(amount) || amount <= 0) {
     alert("Please enter a valid amount");
     return;
   }
 
- console.log(expenseName, amountInput, date);
+  //add expense to the array
+  const expense = { name: expenseName, amount, date };
+  expenses.push(expense);
+  console.log(expenses);
+
+  displayExpenses();
+  // Clear input fields
+  nameInput.value = "";
+  amountInput.value = "";
+  dateInput.value = "";
 });
+
+function displayExpenses() {
+  expenseData.innerHTML = expenses
+    .map(
+      (expense) => `
+       <div class="flex">
+      <p class="expense-history">${expense.name} - $${expense.amount.toFixed(
+        2
+      )} - ${expense.date}</p> 
+       <span><button class="btn-delete">X</button></span>
+       </div>
+    `
+    )
+    .join("");
+}
