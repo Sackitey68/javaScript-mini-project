300729021c4de9adc508c0fc4da3dfe251c66fe6
Sackitey68 <sackitey68@gmail.com>
2025-01-03 18:35:57 +0000

    Add a functionality to add task

--- TodoList/main.js
+++ TodoList/main.js
@@ -1,8 +1,21 @@
 const tasks = document.querySelector(".tasks");
+const addForm = document.querySelector(".add");
 
 //functionality to delete tasks
 tasks.addEventListener("click", function (event) {
   if (event.target.classList.contains("delete")) {
     event.target.parentElement.remove();
+  }
+});
+
+//functionality to add Task
+addForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  addTask = addForm.task.value.trim();
+  if (addTask.length) {
+    tasks.innerHTML += ` <li>
+                        <span>${addTask}</span> <i class="bi bi-trash-fill delete"></i>
+                     </li>`;
   }
 });
